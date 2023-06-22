335b554e9624b8c32d3e3831d503ac1c5f4d5e7e
sultaAann <sultandjavgashev@gmail.com>
2023-06-23 03:33:48 +0500

    ok

--- lesson10/src/main/resources/SOLUTION.sql
+++ lesson10/src/main/resources/SOLUTION.sql
@@ -1,12 +1,2 @@
-SELECT * FROM Subject WHERE id IN (
-    SELECT subject_id FROM Mark GROUP BY subject_id HAVING AVG(mark) > (
-        SELECT AVG(mark) FROM Mark
-        )
-    );
-
-SELECT * FROM Student WHERE id IN (
-    SELECT student_id FROM Payment GROUP BY student_id HAVING SUM(amount) < (
-        SELECT AVG(sum_amount) FROM (SELECT SUM(amount) AS sum_amount FROM Payment GROUP BY student_id)
-        )
-    );
-
+SELECT s.* FROM subject s WHERE s.grade > (SELECT AVG(s2.grade) FROM subject s2);
+SELECT s.id, s.name FROM student s JOIN payment p ON s.id = p.student_id WHERE p.amount < (SELECT AVG(p2.amount) FROM payment p2);
--- lesson8/src/main/resources/SOLUTION.sql
+++ lesson8/src/main/resources/SOLUTION.sql
@@ -1,7 +1,4 @@
-SELECT MIN(BIRTHDAY) FROM STUDENT;
-
-SELECT MIN(PAYMENT_DATE) FROM PAYMENT ORDER BY PAYMENT_DATE DESC LIMIT 1;
-
-SELECT avg(MARK) FROM MARK;
-
-SELECT min(AMOUNT) FROM PAYMENT WHERE TYPE_ID = 'WEEKLY';
+SELECT birthday FROM student ORDER BY birthday DESC LIMIT 1;
+SELECT min(payment_date) FROM payment;
+SELECT AVG(mark) FROM mark JOIN subject ON subject.id = mark.subject_id WHERE subject.name = 'Math';
+SELECT MIN(amount) FROM payment JOIN paymenttype ON paymenttype.id = payment.type_id WHERE paymenttype.name = 'WEEKLY';
